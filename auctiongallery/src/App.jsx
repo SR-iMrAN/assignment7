f215c7731d6bbcd97d3b319317ec22bfb2595020
SR-iMrAN <srimran423@gmail.com>
2025-04-11 19:18:23 +0600

    nav bar setup

--- auctiongallery/src/App.jsx
+++ auctiongallery/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 
 import './App.css'
+import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hello</h1>
+    <Navbar></Navbar>
     </>
   )
 }

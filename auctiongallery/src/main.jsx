0489d312f7bcb60096612ceed7aa507b2403fd77
SR-iMrAN <srimran423@gmail.com>
2025-04-12 13:32:46 +0600

    footer

--- auctiongallery/src/main.jsx
+++ auctiongallery/src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'react-toastify/dist/ReactToastify.css';
-
+import { CiHeart } from "react-icons/ci";
+import { BsHeartFill } from "react-icons/bs";
+import { ImCancelCircle } from "react-icons/im";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
